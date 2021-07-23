a3160393081bef37780f3815dfceeba2953ed42d
Si Ting <33442908+SiTingST@users.noreply.github.com>
2021-07-23 16:18:45 +0800

    connect to database

--- lifehack_bot.py
+++ lifehack_bot.py
@@ -1,18 +1,43 @@
 import logging
+from telegram.ext import (Updater, InlineQueryHandler, CommandHandler,  CallbackQueryHandler, MessageHandler, Filters, ConversationHandler, CallbackContext)
+from telegram.ext.dispatcher import run_async
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+import requests
+import re
+import psycopg2
+from psycopg2 import Error
+import random
+import logging
 import os
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+import math
+import time
+import datetime
+from array import array
 
 PORT = int(os.environ.get('PORT', 5000))
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 TOKEN = '1908824393:AAE3SZKfsySMCu-PZQNqtuiy7Xm4GXKEHsM'
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
+given_keyboard = [['CREATE Deck', 'VIEW deck 👀'],
+                  ['ADD Deck ➕', 'DELETE deck ⛔'],
+                  ['PRACTICE 💪', 'TEST ✍'],
+                  ['COMPETE']]
+
+markup = ReplyKeyboardMarkup(given_keyboard)
+
+
+def connect_db():
+    connection = psycopg2.connect(user="ypuzvnpikuyzec",
+                                  password="b508ea454fb3ea5f4831560152799b203714bc742bb8c4b000fac6b91ef28cec",
+                                  host="ec2-35-171-31-33.compute-1.amazonaws.com",
+                                  port="5432",
+                                  database="dfhns0og19dacd")
+    return connection
+
+
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
@@ -29,25 +54,47 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
+def start(update, context):
+    startMessage = "Click NEW to get started!\n\n"
+    startMessage += "Need help? 🆘\nClick /help to see all available commands and what they do.\n"
+    update.message.reply_text(startMessage, reply_markup=markup)
+    try:
+        connection = connect_db()
+        cursor = connection.cursor()
+        postgres_insert_query = """INSERT INTO users (user_id, users_questions, test_done) VALUES (%s, %s, False) """
+        user_to_insert = (getID(update), [],)
+        cursor.execute(postgres_insert_query, user_to_insert)
+        connection.commit()
+        count = cursor.rowcount
+        print (count, "user inserted successfully into users table")
+    except (Exception, psycopg2.Error):# as error :
+        if (connection):
+            # print("Failed to insert user into users table: ", error)
+            print("User already registered")
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            # print("PostgreSQL connection is closed")
+
+def getID(update):
+    return(update.message.chat.id)
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
 
-    # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
+
+    start()
 
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
@@ -55,10 +102,6 @@ def main():
                           url_path=TOKEN)
 
     updater.bot.setWebhook('https://lifehackbots.herokuapp.com/' + TOKEN)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 if __name__ == '__main__':
