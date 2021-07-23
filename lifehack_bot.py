ebb4abf31ea9c48b08ca1268bfd940796dceb841
Si Ting <33442908+SiTingST@users.noreply.github.com>
2021-07-24 00:27:13 +0800

    updated create deck and play deck

--- lifehack_bot.py
+++ lifehack_bot.py
@@ -6,8 +6,8 @@ from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
-CHOOSING, CREATE_DECK, CREATE_ANSWERS, CREATE_QUESTIONS, PLAY_DECK, VIEW_ALL_DECKS, VIEW_MY_DECKS, LEADERBOARDS, \
-MOTIVATION = range(9)
+CHOOSING, CREATE_DECK, CREATE_ANSWERS, CREATE_QUESTIONS, PLAY_DECK, VIEW_ALL_DECKS, VIEW_MY_DECKS, WAITING_FOR_USER_ANS, \
+GIVE_USER_QUESTION, LEADERBOARDS, MOTIVATION = range(11)
 
 PORT = int(os.environ.get('PORT', 5000))
 # Enable logging
@@ -28,6 +28,7 @@ def generate_random_string():
     deck_token = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
     return deck_token
 
+
 def database_connection():
     con = psycopg2.connect(user="wphtrnjifgtphq",
                            password="c870974f40f5ca10d7f7abcb6cbc4b89137bc612a516a917784990da74c3bd95",
@@ -35,6 +36,7 @@ def database_connection():
                            port="5432",
                            database="dac876p6jjg42g")
     return con
+
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -49,23 +51,22 @@ def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
 def create_deck_message(update, context):
     update.message.reply_text(
         "Want to create a new deck? Please give your new deck a name. \n\nTo cancel, type /cancel.")
     return CREATE_DECK
 
+
 def create_deck(update, context):
     user_input = update.message.text
     deck_token = generate_random_string()
+    context.chat_data["deck_token"] = deck_token
     if user_input == "/cancel":
         cancel(update, context)
         return CHOOSING
     else:
         deck = user_input
+
         try:
             connection = database_connection()
             cursor = connection.cursor()
@@ -82,8 +83,9 @@ def create_deck(update, context):
 
     reply_keyboard = [['Yes', 'No']]
     update.message.reply_text("Proceed to add questions to your new deck?",
-                                reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return CREATE_QUESTIONS
+
 
 def create_questions(update, context):
     user_input = update.message.text
@@ -94,49 +96,152 @@ def create_questions(update, context):
         update.message.reply_text("Submitted!")
         return CHOOSING
     else:
+        if user_input != "Yes":
+            context.chat_data["answer"] = user_input
+
+            try:
+                connection = database_connection()
+                cursor = connection.cursor()
+                questions_data = (
+                    context.chat_data["questions"], context.chat_data["answer"], context.chat_data["deck_token"])
+                insert_query = """INSERT INTO questions (question, answer, deck_token) VALUES (%s, %s, %s) """
+                cursor.execute(insert_query, questions_data)
+                connection.commit()
+                print("Questions and ans created successfully!")
+
+            except (Exception, psycopg2.Error) as e:  # as error :
+                print(format(e))
+
+            cursor.close()
+            connection.close()
+
         update.message.reply_text("Please enter your question.")
         return CREATE_ANSWERS
 
+
 def create_answers(update, context):
     user_input = update.message.text
+    context.chat_data["questions"] = user_input
     update.message.reply_text("Please enter your answers.")
     if user_input == "/submit":
         update.message.reply_text("Submitted!")
         return CHOOSING
     return CREATE_QUESTIONS
 
+
+def check_if_token_is_valid(given_deck_token):
+    try:
+        connection = database_connection()
+        cursor = connection.cursor()
+        print(given_deck_token)
+        select_query = """SELECT COUNT(*) FROM decks where deck_token =(%s) """
+        cursor.execute(select_query, (given_deck_token,))
+        count = cursor.fetchone()
+        # remove tuple from count
+        count_int = str(count)[1:-2]
+        print("checked deck token validity!")
+    except (Exception, psycopg2.Error) as e:  # as error :
+        print(format(e))
+
+    return count_int
+
+
 def play_deck_message(update, context):
     update.message.reply_text("Enter deck token to play!. \n\nTo cancel, type /cancel.")
+    context.chat_data["counter"] = 0
+
     return PLAY_DECK
 
+
+def select_questions_and_answer_from_deck(deck_token):
+    try:
+        connection = database_connection()
+        cursor = connection.cursor()
+        select_query = """SELECT question, answer FROM questions where deck_token =(%s) """
+        cursor.execute(select_query, (deck_token,))
+        questions_and_answers = cursor.fetchall()
+
+    except (Exception, psycopg2.Error) as e:  # as error :
+        print(format(e))
+
+    return questions_and_answers
+
+
 def play_deck(update, context):
-    user_input = update.message.text
-    if (user_input == "/cancel"):
+    deck_token = update.message.text
+    if deck_token == "/cancel":
         cancel(update, context)
         return CHOOSING
     else:
-        update.message.reply_text(user_input)  # echo -> Si Ting TODO
-    return CHOOSING
+        if int(check_if_token_is_valid(deck_token)) == int(0):
+            update.message.reply_text("Please enter a valid deck token. \n\nTo cancel, type /cancel.")
+        else:
+            questions_and_answer = select_questions_and_answer_from_deck(deck_token)
+            context.chat_data["questions_and_ans"] = select_questions_and_answer_from_deck(deck_token)
+            context.chat_data["num_of_qn"] = len(context.chat_data["questions_and_ans"])
+
+            # first question
+            context.chat_data["questions"] = questions_and_answer[context.chat_data["counter"]][0]
+
+            # first answer
+            context.chat_data["answer"] = questions_and_answer[context.chat_data["counter"]][1]
+            context.chat_data["counter"] = context.chat_data["counter"] + 1
+
+        update.message.reply_text(context.chat_data["questions"])
+
+        update.message.reply_text("Please enter your answer.")
+        return WAITING_FOR_USER_ANS
+
+
+def give_user_question(update, context):
+    if context.chat_data["counter"] == context.chat_data["num_of_qn"]:
+        update.message.reply_text("That was the end of the quiz!")
+    else:
+        update.message.reply_text(context.chat_data["questions"])
+        update.message.reply_text("Please enter your answer.")
+
+    return WAITING_FOR_USER_ANS
+
+
+def validate_user_answer(update, context):
+    user_answer = update.message.text
+    if user_answer == context.chat_data["answer"]:
+        update.message.reply_text("Good Job! That's the correct answer!! To cancel, type /cancel.")
+    else:
+        update.message.reply_text("Good Try! But the correct answer is " + context.chat_data["answer"] +
+                                  "  To cancel, type /cancel.")
+    reply_keyboard = [['Yes', 'No']]
+    update.message.reply_text("Proceed to add questions to your new deck?",
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    return GIVE_USER_QUESTION
+
 
 def view_all_decks_message(update, context):
     update.message.reply_text("Here are all available decks. Enter a deck token to play! \n\nTo cancel, type /cancel.")
+    
+
     return PLAY_DECK
+
 
 def view_my_decks_message(update, context):
     update.message.reply_text("Drk what to do here @ST Enter token to view leaderboards? To play?")
     return CHOOSING
 
+
 def cancel(update, context):
     update.message.reply_text("Cancelled!", reply_markup=markup)
     return CHOOSING
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+
 def done(update, context):
     update.message.reply_text("Thanks for playing! Come back soon!")
     return ConversationHandler.END
+
 
 def main():
     updater = Updater(TOKEN, use_context=True)
@@ -158,7 +263,9 @@ def main():
             CREATE_DECK: [MessageHandler(Filters.text, create_deck)],
             PLAY_DECK: [MessageHandler(Filters.text, play_deck)],
             CREATE_QUESTIONS: [MessageHandler(Filters.text, create_questions)],
-            CREATE_ANSWERS: [MessageHandler(Filters.text, create_answers)]
+            CREATE_ANSWERS: [MessageHandler(Filters.text, create_answers)],
+            WAITING_FOR_USER_ANS: [MessageHandler(Filters.text, validate_user_answer)],
+            GIVE_USER_QUESTION: [MessageHandler(Filters.text, play_deck)]
         },
         fallbacks=[CommandHandler('done', done)]
     )
